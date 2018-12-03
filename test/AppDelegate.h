27b7c06e57aa26808f9ad3bd2a46675a354e8e83
leejiaolon mac <lijiaolong2017@163.com>
2018-12-03 15:05:15 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by iMac  on 2018/12/3.
+//  Copyright © 2018 leejiaolongPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
